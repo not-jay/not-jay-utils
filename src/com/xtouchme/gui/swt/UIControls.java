81cffdb4dfb6e530d257ce1ade5c4c71174974e0
not-jay <jardsonsona@yandex.com>
2014-08-13 23:43:53 +0800

    Added 'centerFrame' function

--- src/com/xtouchme/gui/swt/UIControls.java
+++ src/com/xtouchme/gui/swt/UIControls.java
@@ -1,7 +1,12 @@
 package com.xtouchme.gui.swt;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingWorker;
+import javax.swing.UIManager;
 
 /**
  * Contains thread-safe SWT Component update methods
@@ -9,6 +14,21 @@ import javax.swing.SwingWorker;
  */
 public class UIControls {
 
+	public static void systemLookAndFeel() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void centerFrame(JFrame frame, int width, int height) {
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		frame.setBounds(gd.getDisplayMode().getWidth()/2 - width/2,
+						gd.getDisplayMode().getHeight()/2 - height/2,
+						width, height);
+	}
+	
 	public static void update(final JLabel toUpdate, final String newValue) {
 		SwingWorker<Void, String> worker = new SwingWorker<Void, String>() {
 			protected Void doInBackground() throws Exception {
